7ade1b95e5914bf0cb228796458f80422af783e1
Roman Kirillov <sigizmund@gmail.com>
2025-06-07 17:03:51 +0100

    Allowing only one copy of app to run

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,15 @@
 // Sources/main.swift
 import Cocoa
 
+let bundleID = Bundle.main.bundleIdentifier!
+let runningInstances = 
+    NSRunningApplication.runningApplications(withBundleIdentifier: bundleID);
+
+if runningInstances.count > 1 {
+    NSLog("Another instance is already running. Exiting.")
+    exit(0)
+}
+
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
